5db5bfd23aa6693916d70a5e76a867871c1d59d9
กิตติ อำนาจสมบูรณ์ <105689839+kittiumnatsomboon@users.noreply.github.com>
2024-10-25 11:39:15 +0700

    learning

--- src/index.js
+++ src/index.js
@@ -4,13 +4,28 @@ import './index.css';
 import App from './controller/App';
 import reportWebVitals from './reportWebVitals';
 
+let calculate = (sum , sam )=>{
+  
+    if(sum <= sam){
+      
+        console.log("ค่า sum มากกว่า sam")
+    }else{
+        console.log("ค่า sam มากกว่า sum")
+    }
+}
+let xfunction = (x , y)=>{
+    const z = x + y
+    return z
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  calculate(100,6),
+  
 );
-
+const call = ReactDOM.createRoot(document.getElementById('call'));
+call.render(
+  xfunction(156 , 165)
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
