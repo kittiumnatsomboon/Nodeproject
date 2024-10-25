b17dcb29856ffcce4c6658676cc6c092a938707b
กิตติ อำนาจสมบูรณ์ <105689839+kittiumnatsomboon@users.noreply.github.com>
2024-10-25 13:29:03 +0700

    String

--- src/index.js
+++ src/index.js
@@ -4,29 +4,10 @@ import './index.css';
 import App from './controller/App';
 import reportWebVitals from './reportWebVitals';
 
-// let Employee = (fname,lname)=> fname , lname
-// let Employee = (Ename,Elasname)=>alert( Elasname + Ename )
-
-  const customer = {
-      cusmername : "กิตติ",
-      cusmer_lastname : "อำนาจสมบูรณ์",
-      cusmer_add : {
-        county : "thailand",
-        zipcode : 18120,
-        code_h : 20/7
-      },
-      showdata(){
-          console.log("แสดงข้อมูลลูกค้า")
-          console.log(customer.cusmername)
-          console.log(customer.cusmer_lastname)
-          
-      }
-  }
-  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    console.log(customer.showdata())
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
