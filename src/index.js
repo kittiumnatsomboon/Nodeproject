ad32a5e6da273e3e5abfbb3888d6f8de489916b2
กิตติ อำนาจสมบูรณ์ <105689839+kittiumnatsomboon@users.noreply.github.com>
2024-10-25 13:09:59 +0700

    Object

--- src/index.js
+++ src/index.js
@@ -4,28 +4,31 @@ import './index.css';
 import App from './controller/App';
 import reportWebVitals from './reportWebVitals';
 
-const calculate = (sum , sam )=>{
-    let total = sum + sam
-    if(sum <= sam){
-         
-       return console.log("ค่า sum มากกว่า sam" + total)
-    }else{
-        console.log("ค่า sam มากกว่า sum")
-    }
-}
-let xfunction = (x , y)=>{
-    const z = x + y
-    return z
-}
+// let Employee = (fname,lname)=> fname , lname
+// let Employee = (Ename,Elasname)=>alert( Elasname + Ename )
+
+  const customer = {
+      cusmername : "กิตติ",
+      cusmer_lastname : "อำนาจสมบูรณ์",
+      cusmer_add : {
+        county : "thailand",
+        zipcode : 18120,
+        code_h : 20/7
+      },
+      showdata(){
+          console.log("แสดงข้อมูลลูกค้า")
+          console.log(customer.cusmername)
+          console.log(customer.cusmer_lastname)
+          
+      }
+  }
+  
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  calculate(100 , 10),
-  
+    console.log(customer.showdata())
 );
-const call = ReactDOM.createRoot(document.getElementById('call'));
-call.render(
-  xfunction(156 , 165)
-);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
