05f9d6a73a15d091b2d4f8d004115d7cc704ff7d
กิตติ อำนาจสมบูรณ์ <105689839+kittiumnatsomboon@users.noreply.github.com>
2024-10-26 18:21:12 +0700

    Save

--- src/controller/App.js
+++ src/controller/App.js
@@ -1,25 +1,16 @@
 import logo from '../logo.svg';
 import '../App.css'
+import { render } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      he
     </div>
   );
 }
-
+export function Mynameobj(){
+  let emp = "HELLOWORLD"
+ return console.log(emp)
+}
 export default App;
